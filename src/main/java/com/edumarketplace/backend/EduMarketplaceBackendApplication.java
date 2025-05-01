45b39770d6e2ae1d5c27173c61a83b730cbb1378
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-01 10:59:29 +0200

    first commit

---
+++ src/main/java/com/edumarketplace/backend/EduMarketplaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edumarketplace.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduMarketplaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduMarketplaceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edumarketplace/backend/EduMarketplaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumarketplace.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduMarketplaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
