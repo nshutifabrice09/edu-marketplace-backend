ae5dd39c1fb6a85b0d3b16dfd0246107b00b32f7
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-02 11:32:15 +0200

    exceptions-update

--- src/main/java/com/edumarketplace/backend/service/UserService.java
+++ src/main/java/com/edumarketplace/backend/service/UserService.java
@@ -1,2 +1,13 @@
-package com.edumarketplace.backend.service;public class UserService {
+package com.edumarketplace.backend.service;
+
+import com.edumarketplace.backend.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long id);
+    User saveUser (User user);
+    User updateUser(Long id, User user);
+    void removeById(Long id);
 }
