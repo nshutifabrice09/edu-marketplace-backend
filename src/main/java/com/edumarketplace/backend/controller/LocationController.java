826e1c2381495ad4051d67207b6a6ac8272a46dd
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-05 22:16:20 +0200

    third controller-commit

--- src/main/java/com/edumarketplace/backend/controller/LocationController.java
+++ src/main/java/com/edumarketplace/backend/controller/LocationController.java
@@ -4,8 +4,11 @@ import com.edumarketplace.backend.model.Location;
 import com.edumarketplace.backend.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -21,5 +24,10 @@ public class LocationController {
     @PostMapping("/location")
     public Location saveLocation(Location location){
         return locationService.saveLocation(location);
+    }
+
+    @GetMapping("/locations")
+    public List<Location> locationList(){
+        return locationService.getAllLocations();
     }
 }
--- src/main/java/com/edumarketplace/backend/controller/PaymentController.java
+++ src/main/java/com/edumarketplace/backend/controller/PaymentController.java
@@ -1,2 +1,25 @@
-package com.edumarketplace.backend.controller;public class PaymentController {
+package com.edumarketplace.backend.controller;
+
+import com.edumarketplace.backend.model.Payment;
+import com.edumarketplace.backend.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class PaymentController {
+
+    private PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("payment")
+    public Payment savePayment(Payment payment){
+        return paymentService.savePayment(payment);
+    }
 }
