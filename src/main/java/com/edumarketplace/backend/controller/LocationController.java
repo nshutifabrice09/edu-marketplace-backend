d9b70728516610b5a33eb37d9264d1dabb90d27d
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-05 21:43:57 +0200

    second controller-commit

--- src/main/java/com/edumarketplace/backend/controller/LocationController.java
+++ src/main/java/com/edumarketplace/backend/controller/LocationController.java
@@ -1,2 +1,25 @@
-package com.edumarketplace.backend.controller;public class LocationController {
+package com.edumarketplace.backend.controller;
+
+import com.edumarketplace.backend.model.Location;
+import com.edumarketplace.backend.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class LocationController {
+
+    private LocationService locationService;
+
+    @Autowired
+    public LocationController (LocationService locationService){
+        this.locationService = locationService;
+    }
+
+    @PostMapping("/location")
+    public Location saveLocation(Location location){
+        return locationService.saveLocation(location);
+    }
 }
---
+++ src/main/java/com/edumarketplace/backend/controller/PaymentController.java
@@ -0,0 +1,2 @@
+package com.edumarketplace.backend.controller;public class PaymentController {
+}
