56bc0f09c4ffcea1a94b61a86c4b13e4d100f845
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-06 09:18:24 +0200

    updates-service-impl

--- src/main/java/com/edumarketplace/backend/service/SubjectService.java
+++ src/main/java/com/edumarketplace/backend/service/SubjectService.java
@@ -1,4 +1,13 @@
 package com.edumarketplace.backend.service;
 
+import com.edumarketplace.backend.model.Subject;
+
+import java.util.List;
+
 public interface SubjectService {
+    List<Subject> getAllSubjects();
+    Subject getSubjectById(Long id);
+    Subject saveSubject(Subject subject);
+    Subject updateSubject(Long id, Subject subject);
+    void removeSubjectById(Long id);
 }
---
+++ src/main/java/com/edumarketplace/backend/service/SubjectServiceImplementation.java
@@ -0,0 +1,34 @@
+package com.edumarketplace.backend.service;
+
+import com.edumarketplace.backend.model.Subject;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubjectServiceImplementation implements SubjectService{
+    @Override
+    public List<Subject> getAllSubjects() {
+        return null;
+    }
+
+    @Override
+    public Subject getSubjectById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Subject saveSubject(Subject subject) {
+        return null;
+    }
+
+    @Override
+    public Subject updateSubject(Long id, Subject subject) {
+        return null;
+    }
+
+    @Override
+    public void removeSubjectById(Long id) {
+
+    }
+}
